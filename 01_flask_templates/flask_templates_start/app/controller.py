4d5783d46b8d9f9cf20169c7213ef667918ae9af
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-24 13:49:34 +0100

    at end of lesson

--- 01_flask_templates/flask_templates_start/app/controller.py
+++ 01_flask_templates/flask_templates_start/app/controller.py
@@ -1,8 +1,21 @@
 from app import app
-from flask import render_template
-from app.models.todo_list import tasks
+from flask import render_template, request, redirect
+from app.models.todo_list import tasks, add_new_task
+from app.models.task import *
 
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home', tasks=tasks)
+
+
+@app.route('/add-task', methods=['POST'])
+def add_task():
+    task_title = request.form['title']
+    task_description = request.form['description']
+
+    new_task = Task(task_title, task_description, False)
+    add_new_task(new_task)
+
+    return redirect('/')
+
