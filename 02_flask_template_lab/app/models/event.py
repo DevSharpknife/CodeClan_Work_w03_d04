16b9945fca731f3e75975e003147a6023eaf1357
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-24 14:53:00 +0100

    wrote html skeleton

---
+++ 02_flask_template_lab/app/models/event.py
@@ -0,0 +1,11 @@
+class Event()
+
+    def __init__(self, date, name, attendees, location, description):
+        self.date = date
+        self.name = name
+        self.attendees = attendees
+        self.location = location
+        self.description = description
+
+    
+        
---
+++ 02_flask_template_lab/app/models/event_list.py
@@ -0,0 +1,4 @@
+from app.models.event import *
+
+event_1 = Event(20200925, "Codeclan Lab", 2, "CodeClan HQ", "bumbling along nicely")
+event_2 = Event(20200926, "CodeClan Remote", 17, "Home", "all done on zoom")
